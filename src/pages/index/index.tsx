7bd79b152cab84763da59e1cc9164491ee180618
Jack <786322423@qq.com>
2021-01-09 00:50:13 +0800

    login接口测试成功

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
-import { View, Text, Image } from 'remax/wechat';
+import { useNativeEffect } from 'remax';
+import { View, Text, Image, login, request } from 'remax/wechat';
 import styles from './index.css';
 
 export default () => {
+  useNativeEffect(() => {
+    login().then((res) => {
+      console.log(res);
+      request({
+        url: 'http://localhost:8000/user/login',
+        data: res,
+      }).then((res) => {
+        console.log(res);
+      })
+    }).catch((err) => {
+      console.log('login fail', err);
+    })
+  }, [])
+
   return (
     <View className={styles.app}>
       <View className={styles.header}>
